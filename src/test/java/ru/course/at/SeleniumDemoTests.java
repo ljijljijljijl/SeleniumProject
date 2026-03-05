d014d0e33bba58460ec1dcdd06ff186be387b846
ljijljijljijl <dpshevelev@gmail.com>
2026-03-05 22:31:29 +0300

    Added resultsLinkTest

--- src/test/java/ru/course/at/SeleniumDemoTests.java
+++ src/test/java/ru/course/at/SeleniumDemoTests.java
@@ -7,8 +7,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,13 +24,17 @@ public class SeleniumDemoTests {
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
         driver.get("https://www.bing.com/");
     }
 
     @AfterEach
     public void tearDown() {
         driver.quit();
+    }
+
+    public void clickElement(List<WebElement> searchResults, int num) {
+        searchResults.get(num).click();
     }
 
     @Test
@@ -39,5 +47,26 @@ public class SeleniumDemoTests {
 
         WebElement searchPageField = driver.findElement(searchFieldCss);
         assertEquals(input, searchPageField.getAttribute("value"), "Текст не совпадает");
+    }
+
+    @Test
+    public void resultsLinkTest() {
+        String input = "Selenium";
+        By searchFieldCss = By.cssSelector("#sb_form_q");
+        By relevantLink = By.xpath("//a[contains(@class, 'tilk')]//cite[contains(., 'selenium.dev')]");
+        WebElement searchField = driver.findElement(searchFieldCss);
+        searchField.sendKeys(input);
+        searchField.submit();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.elementToBeClickable(relevantLink));
+        List<WebElement> searchResults = driver.findElements(relevantLink);
+        clickElement(searchResults, 0);
+
+        wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        if (tabs.size() > 1) driver.switchTo().window(tabs.get(1));
+        wait.until(ExpectedConditions.urlToBe("https://www.selenium.dev/"));
+        assertEquals("https://www.selenium.dev/", driver.getCurrentUrl(), "Адрес страницы не совпадает с ожидаемым");
     }
 }

2f70adffa40e7d2d6c272999673a50fa9d83d920
ljijljijljijl <dpshevelev@gmail.com>
2026-03-15 18:44:22 +0300

    Changed Bing to Duckduckgo

--- src/test/java/ru/course/at/SeleniumDemoTests.java
+++ src/test/java/ru/course/at/SeleniumDemoTests.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,13 +18,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SeleniumDemoTests {
 
     private WebDriver driver;
+    String input = "Selenium";
+    By searchFieldCss = By.cssSelector("input[class*='search-input']");
+    String seleniumUrl = "https://www.selenium.dev/";
+
+    public void clickElement(List<WebElement> searchResults, int num) {
+        searchResults.get(num).click();
+    }
 
     @BeforeEach
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
-        driver.get("https://www.bing.com/");
+        driver.get("https://www.duckduckgo.com/");
     }
 
     @AfterEach
@@ -33,28 +39,20 @@ public class SeleniumDemoTests {
         driver.quit();
     }
 
-    public void clickElement(List<WebElement> searchResults, int num) {
-        searchResults.get(num).click();
-    }
-
     @Test
     public void searchFieldTest() {
-        String input = "Selenium";
-        By searchFieldCss = By.cssSelector("#sb_form_q");
         WebElement searchField = driver.findElement(searchFieldCss);
         searchField.sendKeys(input);
         searchField.submit();
 
-        WebElement searchPageField = driver.findElement(searchFieldCss);
-        assertEquals(input, searchPageField.getAttribute("value"), "Текст не совпадает");
+        WebElement searchPageField = driver.findElement(By.cssSelector("#search_form_input"));
+        assertEquals(input, searchPageField.getAttribute("value"),
+                "Текст не совпадает");
     }
 
     @Test
     public void resultsLinkTest() {
-        String input = "Selenium";
-        String seleniumUrl = "https://www.selenium.dev/";
-        By searchFieldCss = By.cssSelector("#sb_form_q");
-        By relevantLink = By.xpath("//a[contains(@class, 'tilk')]//cite[contains(., 'selenium.dev')]");
+        By relevantLink = By.cssSelector("#r1-0 [data-testid='result-extras-url-link']");
         WebElement searchField = driver.findElement(searchFieldCss);
         searchField.sendKeys(input);
         searchField.submit();
@@ -64,11 +62,7 @@ public class SeleniumDemoTests {
         List<WebElement> searchResults = driver.findElements(relevantLink);
         clickElement(searchResults, 0);
 
-        wait.until(ExpectedConditions.numberOfWindowsToBe(2));
-        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
-        if (tabs.size() > 1) driver.switchTo().window(tabs.get(1));
-        wait.until(ExpectedConditions.urlToBe(seleniumUrl));
         assertEquals(seleniumUrl, driver.getCurrentUrl(),
-                "Адрес страницы не совпадает с ожидаемым");
+                "Первый отображаемый сайт на странице не https://www.selenium.dev/");
     }
 }
